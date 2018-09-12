254d7a00e8e45f1137d4451528460a596ea37eb4
lance nguyen <lance.duc.nguyen@gmail.com>
2018-09-12 17:41:26 +0000

    use pushd/popd

--- packages/sfdx.sh
+++ packages/sfdx.sh
@@ -1,5 +1,9 @@
 #! /bin/bash
+pushd /tmp/
+
 wget https://developer.salesforce.com/media/salesforce-cli/sfdx-linux-amd64.tar.xz
 mkdir sfdx
 tar xJf sfdx-linux-amd64.tar.xz -C sfdx --strip-components 1
 sudo ./sfdx/install
+
+popd

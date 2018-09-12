99c0de017ffcd736e8da35e90967e76be9f050c4
lance nguyen <lance.duc.nguyen@gmail.com>
2018-09-12 17:38:13 +0000

    add sfdx.sh

---
+++ packages/sfdx.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+wget https://developer.salesforce.com/media/salesforce-cli/sfdx-linux-amd64.tar.xz
+mkdir sfdx
+tar xJf sfdx-linux-amd64.tar.xz -C sfdx --strip-components 1
+sudo ./sfdx/install

63931865c3c524f36fa79b56167197d52fd02909
Lance Duc Nguyen <lance.duc.nguyen@gmail.com>
2018-09-19 23:22:00 -0700

    Update firefoxquantum.sh

--- packages/firefoxquantum.sh
+++ packages/firefoxquantum.sh
@@ -1,19 +1,20 @@
 #! /bin/bash
 
-sudo apt-get install curl
+sudo apt install wget bzip2 
+sudo apt-get install libdbus-glib-1-2
 
 sudo wget -O FirefoxSetup.tar.bz2 "https://download.mozilla.org/?product=firefox-latest&os=linux64&lang=en-US"
-sudo tar -xjf /tmp/firefox.tar.bz2 -C /opt/
+sudo tar xjf FirefoxSetup.tar.bz2
+sudo mv firefox /opt/firefox
+
+sudo ln -s /opt/firefox/firefox /usr/bin/firefox
 
 sudo bash -c 'cat << EOF > /usr/share/applications/firefox.desktop
 [Desktop Entry]
-Version=1.0
-Name=Firefox Developer Edition
+Name=Firefox
 Comment=Browse the World Wide Web
 Icon=/opt/firefox/browser/chrome/icons/default/default128.png
-Exec=/opt/firefox/firefox
-Terminal=false
+Exec=/opt/firefox/firefox %u
 Type=Application
 Categories=Network;WebBrowser;
-Actions=Default;Mozilla;ProfileManager;
 EOF'
